badb772dfbe80fb3e999e72e639c286b66fc6f0a
BANDU-OF-TG <87604582+jithin123455@users.noreply.github.com>
2022-03-03 21:56:57 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -52,7 +52,7 @@ HELP_STRINGS = """
 𝙰𝚗𝚍 𝚝𝚑𝚎 𝚏𝚘𝚕𝚕𝚘𝚠𝚒𝚗𝚐:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\n𝙰𝚕𝚕 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚌𝚊𝚗 𝚎𝚒𝚝𝚑𝚎𝚛 𝚋𝚎 𝚞𝚜𝚎𝚍 𝚠𝚒𝚝𝚑 / 𝚘𝚛 !.\n")
 
-TECHNO_IMG = "https://telegra.ph/file/10cf6a74f348caa0096b9.jpg"
+TECHNO_IMG = "https://telegra.ph/file/60dd05e99074a1893c1e2.jpg"
 IMPORTED = {}
 MIGRATEABLE = []
 HELPABLE = {}
